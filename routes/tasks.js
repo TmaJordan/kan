630aa94ddd672fc83975d76863ac8ebd75764e2b
Thomas Jordan <tmajordan@gmail.com>
2016-07-26 22:15:01 +0100

    Build out other routes #2 https://github.com/TmaJordan/kan/issues/2 Added in other routes for basic task management (Create, Read & Update)

--- routes/tasks.js
+++ routes/tasks.js
@@ -6,12 +6,56 @@ var Task = mongoose.model('Task');
 var Comment = mongoose.model('Comment');
 var Link = mongoose.model('Link');
 
+/* Routes for tasks */
 router.get('/', function(req, res, next) {
   Task.find(function(err, tasks) {
     if (err) {return next(err);}
     
     res.json(tasks);
   })
+});
+
+router.get('/:task', function(req, res) {
+  req.task.populate('comments', function(err, task) {
+    if (err) {return next(err);}
+    
+    res.json(req.task);
+  });
+});
+
+router.put('/:task', function(req, res) {
+    //var updateTask = Object.assign({}, req.task, req.body);
+    for (var attrname in req.body) { req.task[attrname] = req.body[attrname]; }
+
+    req.task.save(function(err, task) {
+        if (err) {return next(err);}
+        
+        res.json(task);
+    });
+});
+
+router.post('/', function(req, res, next) {
+  var task = new Task(req.body);
+  
+  task.save(function(err, task) {
+    if (err) {return next(err);}
+    
+    res.json(task);
+  })
+});
+
+/*Param method intercepts :post for above requests */
+router.param('task', function (req, res, next, id) {
+  var query = Task.findById(id);
+  
+  query.exec(function(err, task) {
+    if (err) {return next(err);}
+    
+    if (!task) {return next(new Error("Can't find task"));}
+    
+    req.task = task;
+    return next(); 
+  }); 
 });
 
 module.exports = router;
